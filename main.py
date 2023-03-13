3205ceafe964fd8d121f9b80be542fd2e619ddad
VSollProd <126521538+VSollProd@users.noreply.github.com>
2023-03-13 18:13:12 +0200

    Update main.py

--- main.py
+++ main.py
@@ -6,12 +6,15 @@ import parseLevel
 import parseAct
 import datetime
 import asyncio
+import pytz
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
+
 
 BOT_TOKEN = '5813656566:AAFXayBd7gVLvn5cAQgvJpUlwbg11r82HPg'
 import time
 
 counter_act = 0
-
+kiev = pytz.timezone('Europe/Kiev')
 counter1 = 0
 counter2 = 0
 counter3 = 0
@@ -22,10 +25,13 @@ parserLvl = parseLevel.WordsParser()
 count_stop = 0
 now = datetime.datetime.now()
 
-tim = f'{now.hour}:{now.minute}'
+
 
 bot = Bot(BOT_TOKEN)
 dp = Dispatcher(bot)
+scheduler = AsyncIOScheduler(timezone=kiev)
+
+
 
 CHANGE_KEYBOARD = ReplyKeyboardMarkup(resize_keyboard=True).add(
     KeyboardButton("/Змінити рівень або сферу діяльності")
@@ -55,11 +61,9 @@ MAIN_KEYBOARD = ReplyKeyboardMarkup(resize_keyboard=True).add(
 
 @dp.message_handler(commands='start')
 async def start_handler(message: types.Message):
-    global count_stop
     await bot.send_message(message.from_user.id,
                            "Привіт, це бот для вивчення аглійських слів, тут можно обрати свою сферу або свій рівень англійскої мови(А1,В2.....). Обери нижче! \n Після того як оберете тип слів що вам треба вивчати вам буде приходити 5 слів кожний день о 12:00 день.",
                            reply_markup=MAIN_KEYBOARD)
-    count_stop+=1
 
 
 @dp.message_handler(commands='0брати')
@@ -85,15 +89,20 @@ async def level_handler(message: types.Message):
 
 
     if level == 'A1':
-        await a1(message)
+        scheduler.add_job(a1, args=(message,), trigger='cron', hour = 18 , minute = 9)
+
     elif level == 'A2':
-        await a2(message)
+        scheduler.add_job(a2,args=(message,), trigger='cron', hour = 18 , minute = 11)
+
     elif level == 'B1':
-        await b1(message)
+        scheduler.add_job(b1,args=(message,), trigger='cron', hour = 12 , minute = 0)
+
     elif level == 'B2':
-        await b2(message)
+        scheduler.add_job(b2,args=(message,), trigger='cron', hour = 12 , minute = 0)
+
     elif level == 'C1/2':
-        await c1(message)
+        scheduler.add_job(c1,args=(message,), trigger='cron', hour = 12 , minute = 0)
+
 
     await bot.send_message(message.from_user.id,
                            f"Ви обрали рівень {level}, тепер вам буде приходить кожен день по 5 слів рівня{level}",
@@ -105,16 +114,18 @@ async def act_handler(message: types.Message):
     act = message.text.split(' ')[1]  # Получаем сферу из текста сообщения
     global counter_act
 
-    await sfera(message, act)
+    # sfera(message, act)
     await bot.send_message(message.from_user.id,
                                f"Ви обрали сферу {act}, тепер вам буде приходить кожен день по 5 слів зі сфери {act}",
                                reply_markup=CHANGE_KEYBOARD)
+    scheduler.add_job(sfera_chng,args=(message, act), trigger='cron', hour=12, minute=0)
 
 
 
-async def sfera(message : types.Message, act):
+
+def sfera(act):
     global count_stop
-    global counter1
+
     list = []
     if act == 'ІТ':
         obj = parseAct.ParseAct()
@@ -131,123 +142,86 @@ async def sfera(message : types.Message, act):
     if act == 'мистецтво':
         obj = parseAct.ParseAct()
         list = obj.parse_art()
-    while True:
-        now = datetime.datetime.now()
-        tim = now.strftime('%H:%M')
+    return list
 
-        if tim == '12:00':
-            if counter1+5 <= len(list):
-                await bot.send_message(message.from_user.id, list[counter1])
-                await bot.send_message(message.from_user.id, list[counter1 + 1])
-                await bot.send_message(message.from_user.id, list[counter1 + 2])
-                await bot.send_message(message.from_user.id, list[counter1 + 3])
-                await bot.send_message(message.from_user.id, list[counter1 + 4])
-                await asyncio.sleep(60)
-                counter1 += 5
-        if message.text == '/Змінити рівень або сферу діяльності':
-            print(1)
-            break
+
+async def sfera_chng(message : types.Message, act):
+    global counter1
+    list = sfera(act)
+    print('ds')
+    await bot.send_message(message.from_user.id, list[counter1])
+    await bot.send_message(message.from_user.id, list[counter1 + 1])
+    await bot.send_message(message.from_user.id, list[counter1 + 2])
+    await bot.send_message(message.from_user.id, list[counter1 + 3])
+    await bot.send_message(message.from_user.id, list[counter1 + 4])
+    await asyncio.sleep(60)
+    counter1 += 5
+
 
 
 async def a1(message: types.Message):
     global count_stop
     global counter1
     list = parserLvl.parse_words('A1')
-    while True:
-        now = datetime.datetime.now()
-        tim = now.strftime('%H:%M')
+    await bot.send_message(message.from_user.id, list[counter1])
+    await bot.send_message(message.from_user.id, list[counter1 + 1])
+    await bot.send_message(message.from_user.id, list[counter1 + 2])
+    await bot.send_message(message.from_user.id, list[counter1 + 3])
+    await bot.send_message(message.from_user.id, list[counter1 + 4])
+    await asyncio.sleep(60)
 
-        if tim == '12:00':
-            await bot.send_message(message.from_user.id, list[counter1])
-            await bot.send_message(message.from_user.id, list[counter1 + 1])
-            await bot.send_message(message.from_user.id, list[counter1 + 2])
-            await bot.send_message(message.from_user.id, list[counter1 + 3])
-            await bot.send_message(message.from_user.id, list[counter1 + 4])
-            await asyncio.sleep(60)
-            counter1 += 5
-        if count_stop % 2 !=0:
-            break
+    counter1 += 5
+
 
 
 async def a2(message: types.Message):
     global count_stop
     global counter2
     list = parserLvl.parse_words('A2')
-    while True:
-        now = datetime.datetime.now()
-        tim = now.strftime('%H:%M')
-        if tim == '12:00':
-            await bot.send_message(message.from_user.id, list[counter2])
-            await bot.send_message(message.from_user.id, list[counter2 + 1])
-            await bot.send_message(message.from_user.id, list[counter2 + 2])
-            await bot.send_message(message.from_user.id, list[counter2 + 3])
-            await bot.send_message(message.from_user.id, list[counter2 + 4])
-            await asyncio.sleep(60)
-            counter2 += 5
-        if count_stop % 2 !=0:
-            print(1)
-            break
+    await bot.send_message(message.from_user.id, list[counter1])
+    await bot.send_message(message.from_user.id, list[counter1 + 1])
+    await bot.send_message(message.from_user.id, list[counter1 + 2])
+    await bot.send_message(message.from_user.id, list[counter1 + 3])
+    await bot.send_message(message.from_user.id, list[counter1 + 4])
+    await asyncio.sleep(60)
 
 
 async def b1(message: types.Message):
     global count_stop
     global counter1
     list = parserLvl.parse_words('B1')
-    while True:
-        now = datetime.datetime.now()
-        tim = now.strftime('%H:%M')
-        if tim == '12:00':
-            await bot.send_message(message.from_user.id, list[counter3])
-            await bot.send_message(message.from_user.id, list[counter3 + 1])
-            await bot.send_message(message.from_user.id, list[counter3 + 2])
-            await bot.send_message(message.from_user.id, list[counter3 + 3])
-            await bot.send_message(message.from_user.id, list[counter3 + 4])
-            await asyncio.sleep(60)
-            counter1 += 5
-        if count_stop % 2 !=0:
-            print(1)
-            break
+    await bot.send_message(message.from_user.id, list[counter1])
+    await bot.send_message(message.from_user.id, list[counter1 + 1])
+    await bot.send_message(message.from_user.id, list[counter1 + 2])
+    await bot.send_message(message.from_user.id, list[counter1 + 3])
+    await bot.send_message(message.from_user.id, list[counter1 + 4])
+    await asyncio.sleep(60)
 
 
 async def b2(message: types.Message):
+
     global count_stop
     global counter4
     list = parserLvl.parse_words('B2')
-    while True:
-        now = datetime.datetime.now()
-        tim = now.strftime('%H:%M')
-        if tim == '12:00':
-            await bot.send_message(message.from_user.id, list[counter4])
-            await bot.send_message(message.from_user.id, list[counter4 + 1])
-            await bot.send_message(message.from_user.id, list[counter4 + 2])
-            await bot.send_message(message.from_user.id, list[counter4 + 3])
-            await bot.send_message(message.from_user.id, list[counter4 + 4])
-            await asyncio.sleep(60)
-            counter4 += 5
-        if count_stop % 2 !=0:
-            print(1)
-            break
+    await bot.send_message(message.from_user.id, list[counter1])
+    await bot.send_message(message.from_user.id, list[counter1 + 1])
+    await bot.send_message(message.from_user.id, list[counter1 + 2])
+    await bot.send_message(message.from_user.id, list[counter1 + 3])
+    await bot.send_message(message.from_user.id, list[counter1 + 4])
+    await asyncio.sleep(60)
 
 
 async def c1(message: types.Message):
     global count_stop
     global counter5
     list = parserLvl.parse_words('C1')
-    while True:
-        now = datetime.datetime.now()
-        tim = now.strftime('%H:%M')
-        if tim == '12:00':
-            await bot.send_message(message.from_user.id, list[counter5])
-            await bot.send_message(message.from_user.id, list[counter5 + 1])
-            await bot.send_message(message.from_user.id, list[counter5 + 2])
-            await bot.send_message(message.from_user.id, list[counter5 + 3])
-            await bot.send_message(message.from_user.id, list[counter5 + 4])
-            await asyncio.sleep(60)
-            counter5 += 5
-        if count_stop % 2 !=0:
-            print(1)
-            break
+    await bot.send_message(message.from_user.id, list[counter1])
+    await bot.send_message(message.from_user.id, list[counter1 + 1])
+    await bot.send_message(message.from_user.id, list[counter1 + 2])
+    await bot.send_message(message.from_user.id, list[counter1 + 3])
+    await bot.send_message(message.from_user.id, list[counter1 + 4])
+    await asyncio.sleep(60)
 
 
-
+scheduler.start()
 executor.start_polling(dp, skip_updates=True)
