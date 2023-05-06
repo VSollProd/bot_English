ffb1274469ab3a7c3b9f195ace3653a68aef48c4
VSollProd <126521538+VSollProd@users.noreply.github.com>
2023-05-06 10:22:01 +0300

    Update main.py

--- main.py
+++ main.py
@@ -27,13 +27,7 @@ parserLvl = parseLevel.WordsParser()
 
 bot = Bot(BOT_TOKEN)
 dp = Dispatcher(bot)
-scheduler = AsyncIOScheduler(timezone=kiev)
-scheduler2 = AsyncIOScheduler(timezone=kiev)
-scheduler3 = AsyncIOScheduler(timezone=kiev)
-scheduler4 = AsyncIOScheduler(timezone=kiev)
-scheduler5 = AsyncIOScheduler(timezone=kiev)
-scheduler6 = AsyncIOScheduler(timezone=kiev)
-scheduler7 = AsyncIOScheduler(timezone=kiev)
+
 
 ids = []
 acts = []
@@ -91,73 +85,63 @@ async def stop(message: types.Message):
 @dp.message_handler(commands='level')
 async def level_handler(message: types.Message):
     level = message.text.split(' ')[1]  # Получаем уровень из текста сообщения
-    user_id = message.from_user.id
 
-    cursor.execute('INSERT OR REPLACE INTO users (user_id, category) VALUES (?, ?)', (user_id, level))
-    conn.commit()
 
+
+    list = parseLevel.parse_words(level)
     await bot.send_message(message.from_user.id,
-                           f"Ви обрали рівень {level}, тепер вам буде приходить кожен день по 5 слів рівня{level}",
+                           level
+                           ,
                            reply_markup=CHANGE_KEYBOARD)
 
 
 @dp.message_handler(commands='Сфера')
 async def act_handler(message: types.Message):
     act = message.text.split(' ')[1]  # Получаем сферу из текста сообщения
-    user_id = message.from_user.id
-    cursor1.execute('INSERT OR REPLACE INTO users2 (user_id, category) VALUES (?, ?)', (user_id, act))
-    conn1.commit()
+
+    list = parseAct.parse_words(act)
     await bot.send_message(message.from_user.id,
-                           f"Ви обрали сферу {act}, тепер вам буде приходить кожен день по 5 слів зі сфери {act}",
+                           list,
                            reply_markup=CHANGE_KEYBOARD)
 
 
-async def send_words():
-    cursor.execute("SELECT user_id FROM users")
-    for i in cursor.fetchall():
-        ids.append(i[0])
+# async def send_words():
+#     cursor.execute("SELECT user_id FROM users")
+#     for i in cursor.fetchall():
+#         ids.append(i[0])
 
-    cursor.execute("SELECT category FROM users")
-    for i in cursor.fetchall():
-        acts.append(i[0])
+#     cursor.execute("SELECT category FROM users")
+#     for i in cursor.fetchall():
+#         acts.append(i[0])
 
-    for k in range(0, len(ids)):
-        print(k)
-        global counter5
-        list = parserLvl.parse_words(acts[k])
-        sample = random.sample(list, 5)
-        for i in sample:
-            await bot.send_message(ids[k], i)
-        await asyncio.sleep(60)
-
-
-async def sfera_chng():
-    cursor1.execute("SELECT user_id FROM users2")
-    for i in cursor1.fetchall():
-        ids.append(i[0])
-
-    cursor1.execute("SELECT category FROM users2")
-    for i in cursor1.fetchall():
-        acts.append(i[0])
-
-    for k in range(0, len(ids)):
-        global counter5
-        list = parserLvl.parse_words(acts[k])
-        sample1 = random.sample(list, 5)
-        for i in sample1:
-            await bot.send_message(ids[k], i)
-        await asyncio.sleep(60)
+#     for k in range(0, len(ids)):
+#         print(k)
+#         global counter5
+        
+#         sample = random.sample(list, 5)
+#         for i in sample:
+#             await bot.send_message(ids[k], i)
+        
 
 
-async def scheduler_runner():
-    scheduler7.add_job(sfera_chng, trigger='interval', days=1)
-    scheduler.add_job(send_words, trigger='interval', days=1)
-    await scheduler7.start()
-    await scheduler.start()
+# async def sfera_chng():
+#     cursor1.execute("SELECT user_id FROM users2")
+#     for i in cursor1.fetchall():
+#         ids.append(i[0])
+
+#     cursor1.execute("SELECT category FROM users2")
+#     for i in cursor1.fetchall():
+#         acts.append(i[0])
+
+#     for k in range(0, len(ids)):
+#         global counter5
+#         list = parserLvl.parse_words(acts[k])
+#         sample1 = random.sample(list, 5)
+#         for i in sample1:
+#             await bot.send_message(ids[k], i)
+        
 
 
-if __name__ == '__main__':
-    loop = asyncio.get_event_loop()
-    executor = loop.create_task(dp.start_polling())
-    loop.run_until_complete(executor)
-    loop.create_task(scheduler_runner())
+
+
+executor.start_polling()
